6f8ac3d925711d0c83dca2997ab691ef6c382bc8
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-16 08:25:33 -0800

    fix: comment the reversal word and sentence strings

--- reverse_words.py
+++ reverse_words.py
@@ -1,18 +1,29 @@
 def reverse_sentences(sentence: str) -> str:
-    
+    # One liner
+    # test = ' '.join(sentence.split()[::-1])
+
+    # splitting the new list of strings
     sentence_arr = sentence.split()
+
+    #reading the length of the array for iteration purposes
     sentence_len = len(sentence_arr) - 1
 
     reversed_sentence = ""
+    
+    #iteration of indexes of array
     for word_index in range(sentence_len, -1, -1):
-        reversed_sentence += ' ' + sentence_arr[word_index] + ' '
 
+        #concatenation of new reversed string
+        reversed_sentence += sentence_arr[word_index] + ' '
+
+    #returning the new reversed statement
     return reversed_sentence.strip()
 
 
 def string_reversal(words: str) -> str:
-    reversed_chars = ""
-    reversed_chars += words[::-1]
+    #inspecting the string from left to right
+    reversed_chars = words[::-1]
+    #returning the new reversed characters
     return reversed_chars
 
 def main():
