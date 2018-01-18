1a2845254f683e5d947d4ffdf26037a790a9b609
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-17 17:53:48 -0800

    chore: complete histogram as a dictionary

--- histogram_dictionary.py
+++ histogram_dictionary.py
@@ -3,9 +3,6 @@ Visualizing histograms
 original_str = 'one fish two fish three fish four fish'
 dict_histogram_of_original_str = {one: 1, fish: 4, two: 1, three: 1, four: 1}
 
->* A unique_words() function that takes a histogram argument and returns the total count of unique words 
-in the histogram. For example, when given the histogram for The Adventures of Sherlock Holmes, it returns 
-the integer 8475.
 >* A frequency() function that takes a word and histogram argument and returns the number of times that 
 word appears in a text. For example, when given the word "mystery" and the Holmes histogram, it will 
 return the integer 20.
@@ -35,23 +32,37 @@ def histogram(str_words: str) -> dict:
     return dict_histogram
 
 
-def unique_words(d_histogram) -> int:
+def unique_words(d_histogram: dict) -> int:
+    #counter tracker for unique words
     count_values = 0
-    #print(d_histogram)
 
+    #loop the content of the dictionary
     for key, value in d_histogram.items():
-        #print(value)
+        #condition the values of histogram to obtain non-repeating ones
         if value == 1:
-            count_values += int(value)
+            count_values += value
             
     #print(count_values)
     
     return count_values
 
+def frequency(find_word: str, wrd_histogram: dict) -> int:
+    count_find_word = 0
+
+    #loop the content of the dictionary to find_word as a dict key
+    for key, value in wrd_histogram.items():
+        #search the keyword within the histogram dictionary
+        if key == find_word:
+            #assign value of the found key word to the count_find_word
+            count_find_word = value
+    
+    return count_find_word
 
 def main():
     str_words = 'one fish two fish three fish four fish'
-    histogram(str_words)
+    main_functionality = histogram(str_words)
+    histogram_freq = frequency('fish', main_functionality)
+    print(histogram_freq)
 
 if __name__ == "__main__":
     main()
