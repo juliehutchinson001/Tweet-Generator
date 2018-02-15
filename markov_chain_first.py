b4843f4f3a5cd4779b85913f6fd681f956f1c05d
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-14 21:49:06 -0800

    init: setup and importing of modules for tweet generator

---
+++ markov_chain_first.py
@@ -0,0 +1,21 @@
+# '''
+# You'll need to write code to do two things:
+
+# Learn a Markov chain from a corpus. You've already written code to find how often a token 
+# appears in a corpus (histogram_dictionary), but now you need to find how often a token appears 
+# after another token.
+
+# Do a random walk on a Markov chain. This should be pretty simple if you pick a good way to 
+# store the Markov chain you learn.
+
+# Make sure to think about what data structures to use to make your code efficient. Both learning 
+# the Markov chain and taking a step of the random walk should take time at most linear in 
+# the size of the corpus.
+# '''
+import random
+import re
+from stochastic_sampling import get_random_word
+from histogram_dictionary import Histogram
+from markov_words_helper import get_long_words, get_words, get_short_words
+
+
