24e71d41bcf7aecb01bae978978b15775275a1eb
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-25 15:08:29 -0700

    feat: implementation of regular expressions to clean up text

--- markov_words_helper.py
+++ markov_words_helper.py
@@ -1,18 +1,36 @@
+import re
+import string
+
 def get_long_words():
     dictionary_long_words = ''
     with open('test_corpus.txt') as dictionary_long_file:
-        dictionary_long_words += dictionary_long_file.read().replace('\n', ' ')
+        dictionary_long_words += dictionary_long_file.read().lower().replace('\n', ' ')
+        remove_punctuation(dictionary_long_words)
+        result_long = []
 
-    return dictionary_long_words
+        reg_long_expressions = re.findall("[A-z]+\'?[A-z]*|\$[0-9]*", dictionary_long_words)
+
+    remove_punctuation(dictionary_long_words)
 
 def get_words():
     dictionary_words = ''
     with open('short_version.txt') as dictionary_file:
-        dictionary_words += dictionary_file.read().replace('\n', ' ')
+        dictionary_words += dictionary_file.read().lower().replace('\n', ' ')
+        remove_punctuation(dictionary_words)
+        result = []
 
-    return dictionary_words
+        reg_expressions = re.findall("[A-z]+\'?[A-z]*|\$[0-9]*", dictionary_words)
+
+    remove_punctuation(dictionary_words)
 
 def get_short_words():
     corpus = 'one fish two fish three fish four fish red fish blue fish'
 
     return corpus
+
+def remove_punctuation(sample_text):
+    remove_text = re.sub('[,.()]', '', remove_text)
+    remove_text = re.sub('--', ' ', remove_text)
+    remove_text = re.sub(':', ' ', remove_text)
+    remove_text = re.sub('/(?<!\S).(?!\S)\s*/', '', remove_text)
+    return remove_text
