9f6b9a46215e0a677e696de98f908b465004a35f
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-26 11:40:42 -0700

    refactor: implement cleaning of text for punctuation purposes

--- markov_words_helper.py
+++ markov_words_helper.py
@@ -7,36 +7,30 @@ def get_long_words():
         dictionary_long_words += dictionary_long_file.read().lower().replace('\n', ' ')
         result_long = []
 
-        reg_long_expressions = re.findall("[A-z]+\'?[A-z]*|\$[0-9]*", dictionary_long_words)
+        #this function cleans my punctuation
+        clean = remove_punctuation(dictionary_long_words)
+        
+        #this method returns each of my words in a list
+        no_regex = re.findall("[A-z]+\'?[A-z]*|\$[0-9]*", clean)
 
-    remove_punctuation(dictionary_long_words)
-
-    for found_reg_exp in reg_long_expressions:
+    #this loop puts all my words together
+    for found_long_reg_exp in no_regex:
         result_long.append(found_long_reg_exp)
-    return result_long
 
-def get_words():
-    dictionary_words = ''
-    with open('short_version.txt') as dictionary_file:
-        dictionary_words += dictionary_file.read().lower().replace('\n', ' ')
-        result = []
-
-        reg_expressions = re.findall("[A-z]+\'?[A-z]*|\$[0-9]*", dictionary_words)
-
-    remove_punctuation(dictionary_words)
-
-    for found_reg_exp in reg_expressions:
-        result.append(found_reg_exp)
-    return result
+    #returns a string of words with no punctuation
+    return str(result_long)
 
 def get_short_words():
     corpus = 'one fish two fish three fish four fish red fish blue fish'
 
     return corpus
 
+# This function is replacing all of my special chars with spaces
 def remove_punctuation(sample_text):
-    remove_text = re.sub('[,.()]', '', remove_text)
-    remove_text = re.sub('--', ' ', remove_text)
-    remove_text = re.sub(':', ' ', remove_text)
-    remove_text = re.sub('/(?<!\S).(?!\S)\s*/', '', remove_text)
-    return remove_text
+    remove_text = re.sub('[,.()]', '', sample_text)
+    remove2_text = re.sub('--', ' ', remove_text)
+    remove3_text = re.sub(':', ' ', remove2_text)
+    remove4_text = re.sub('/(?<!\S).(?!\S)\s*/', '', remove3_text)
+    
+    #text cleaned with no words
+    return remove4_text
