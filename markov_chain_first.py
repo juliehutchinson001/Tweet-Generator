386fbf3d871e7e13b5cfe7b14c354e5f9f41757c
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-26 11:41:19 -0700

    fix: finished the tweet generator

--- markov_chain_first.py
+++ markov_chain_first.py
@@ -1,15 +1,13 @@
 
 import random
+import re
 import string
 from stochastic_sampling import get_random_word
-from histogram_dictionary import Histogram
-from markov_words_helper import remove_punctuation, get_long_words, get_words, get_short_words
+from markov_words_helper import get_long_words, get_short_words
 
 #accessing the list of words as string
 helper_dictionary_words = get_long_words()
-# helper_dictionary_words = get_words()
 # helper_dictionary_words = get_short_words()
-
 
 def first_order_markov(str_words):
     tokens = str_words.split(" ")
@@ -33,43 +31,6 @@ def first_order_markov(str_words):
     
     return markov_dict
 
-def clean_text (corpus):
-    # '''time complexity of this operation: O(k * n * Avg_Word)
-    # where k = symbols
-    #       n = corpus
-    #       Avg_word = length of word
-
-    # result = re.sub(pattern, repl, string, count=0, flags=0);
-
-    # def my_replace(m):
-    #     if :
-    #     return <replacement variant 1>
-    #     return <replacement variant 2>
-
-
-    # result = re.sub("\w+", my_replace, input)
-
-    # '''
-
-    # symbols = '''!?,;'" -- - :'''
-    # clean_string = ''
-
-    # for word in corpus:
-    #     for rev_symbol in symbols:
-    #         clean_string += word.replace(rev_symbol, '')
-
-    # return word
-
-    # remove_punctuation(word_list)
-    # result_list = []
-
-    # items = re.findall("[A-z]+\'?[A-z]*\$[0-9]*", word_list)
-    # for item in items:
-    #     result_list.append(item)
-    # return result_list
-
-    pass
-
 def tweet_generator(order, markov_dict):
     
     final_sentence = ''
@@ -87,31 +48,32 @@ def tweet_generator(order, markov_dict):
         #call my random_word function from my stochastic sampling
         #and index that to my random word
         random_word_sentence = get_random_word(markov_dict[first_word])
-        #append to my sentence string
-        final_sentence += random_word_sentence + ' '
-
+        
         first_word = random_word_sentence
+
+        #append to my sentence string
+        final_sentence += random_word_sentence 
+        
+        sentence = ''.join(final_sentence) 
 
         counter += 1
 
-    return final_sentence.strip()
+    remove_more = re.sub(",", '', sentence)
+    remove_even_more = re.sub("'", ' ', remove_more)
+    # print(remove_even_more)
+    # print("test: " + sentence[0])
+    
+    return remove_even_more.strip()
+    # return final_sentence
 
 
 if __name__ == "__main__":
 
-    #corpus = 'one fish two fish three fish four fish red fish blue fish'
-    clean_data = clean_text(get_words())
+    clean_data = get_long_words()
 
-    histogram = Histogram(clean_data)
-# if you want to return the output of run
-
-
-    # markov_chn_dict = first_order_markov(get_long_words())
     markov_chn_dict = first_order_markov(clean_data)
-    # markov_chn_dict = first_order_markov(get_short_words())
 
-
-    print(tweet_generator(100, markov_chn_dict))
+    print(tweet_generator(10, markov_chn_dict))
 
 
 
